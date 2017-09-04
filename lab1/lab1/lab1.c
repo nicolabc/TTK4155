182c5baa64e164f63d3c4292857db6061ed27fe5
unknown <nicolabc@itk-d652.win.ntnu.no>
2017-09-04 13:34:09 +0200

    Added led flashing

--- lab1/lab1/lab1.c
+++ lab1/lab1/lab1.c
@@ -7,11 +7,20 @@
 
 
 #include <avr/io.h>
+#include "util/delay.h"
+
 
 int main(void)
 {
+	DDRA |= (1<<PA1); // setter PA1 til output
+	PORTA |= (1<<PA1); // setter PA1 høy (så det blir 5V på utgangen til PA1)
+	
     while(1)
     {
+		PORTA &= ~(1<<PA1);
         //TODO:: Please write your application code 
-    }
+		_delay_ms(3000);
+		PORTA |= (1<<PA1);
+		_delay_ms(3000);
+	}
 }
