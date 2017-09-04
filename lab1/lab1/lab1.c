ce6790beba574150725c861a937c5bc9e5ac7abe
unknown <nicolabc@itk-d652.win.ntnu.no>
2017-09-04 09:03:50 +0200

    Atmel filer lab1

---
+++ lab1/lab1/lab1.c
@@ -0,0 +1,17 @@
+/*
+ * lab1.c
+ *
+ * Created: 04.09.2017 08:51:27
+ *  Author: nicolabc
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
