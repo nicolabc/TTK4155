f67181733d25898ee1b5151dbfd5e9455ed918f5
unknown <nicolabc@itk-d652.win.ntnu.no>
2017-09-08 14:07:24 +0200

    Lab 2 Oppgave 1 ferdig
    En fin lite tekst

--- lab1/lab1/lab1.c
+++ lab1/lab1/lab1.c
@@ -17,13 +17,26 @@
 #define FOSC 4915200// Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
+#define set_bit( reg, bit ) (reg |= (1 << bit))
+#define clear_bit( reg, bit ) (reg &= ~(1 << bit))
+#define test_bit( reg, bit ) (reg & (1 << bit))
+//#define loop_until_bit_is_set( reg, bit ) while( !test_bit( reg, bit ) )
+//#define loop_until_bit_is_clear( reg, bit ) while( test_bit( reg, bit ) )
 
 
 int main(void)
 {
 	USART_Init ( MYUBRR );
-	DDRA |= (1<<PA1); // setter PA1 til output
-	PORTA |= (1<<PA1); // setter PA1 høy (så det blir 5V på utgangen til PA1)
+	set_bit(DDRA,PA1);	// setter PA1 til output
+	clear_bit(PORTA,PA1);	 // setter PA1 høy (så det blir 5V på utgangen til PA1)
+	
+	set_bit(DDRE, PE1); //Lab 2, setter PE1 til output (Tror vi)
+	set_bit(PORTE, PE1); //Lab 2, setter PE1 til høy (Latch enable)
+	
+	
+	//DDRA |= (1<<PA1); // setter PA1 til output
+	//PORTA |= (1<<PA1); // setter PA1 høy (så det blir 5V på utgangen til PA1)
+	
 	
     while(1)
     {
